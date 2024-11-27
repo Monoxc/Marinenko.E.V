7f5fcf7c44f938de03072878693031e9ab8a914a
Evgen M.V <marnenkozena@gmail.com>
2024-11-28 00:10:05 +0200

    Update main2.cpp

--- C++/lab5 /main2.cpp
+++ C++/lab5 /main2.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+
+    setlocale(LC_ALL, "RU");
+
+    int sum = 0; 
+    int num;    
+
+ 
+    for (int i = 1; i <= 10; i++) {
+        cout << "Введите натуральное число #" << i << ": ";
+        cin >> num;
+
+      
+        if (num <= 0) {
+            cout << "Ошибка: число должно быть натуральным (больше нуля). Повторите ввод." << endl;
+            i--; 
+            continue;
+        }
+
+        sum += num * num; 
+    }
+
+    
+    cout << "Сумма квадратов введенных чисел: " << sum << endl;
+
+    return 0;
+}
