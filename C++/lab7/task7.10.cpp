8b18011e21c585dd0ffe805cd000786b877370ab
Evgen M.V <marnenkozena@gmail.com>
2024-11-28 00:31:09 +0200

    Update task7.10.cpp

--- C++/lab7/task7.10.cpp
+++ C++/lab7/task7.10.cpp
@@ -1,1 +1,44 @@
+#include <iostream>
+using namespace std;
 
+int main(void) {
+    int n;  
+
+ 
+    cout << "Enter a positive integer value greater than 1 (maximum 20): ";
+    cin >> n;
+
+   
+    if (n <= 1) {
+        cout << "The entered value must be greater than 1." << endl;
+        return 1;  
+    } else if (n > 20) {  
+        cout << "Sorry, the side size is too big." << endl;
+        return 1;  
+    }
+
+    
+    cout << '+';
+    for (int i = 0; i < n; i++) {
+        cout << '-';
+    }
+    cout << '+' << endl;
+
+   
+    for (int i = 0; i < n; i++) {
+        cout << '|';
+        for (int j = 0; j < n; j++) {
+            cout << ' ';  
+        }
+        cout << '|' << endl;
+    }
+
+
+    cout << '+';
+    for (int i = 0; i < n; i++) {
+        cout << '-';
+    }
+    cout << '+' << endl;
+
+    return 0;  
+}
