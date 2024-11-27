a1829f6a96221b66f4dfbc6efc03bedae958a570
Evgen M.V <marnenkozena@gmail.com>
2024-11-27 23:57:50 +0200

    Update main.cpp

--- C++/lab4/main.cpp
+++ C++/lab4/main.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    bool answer;
+    int value;
+    cout << "Enter a value: ";
+    cin >> value;
+    
+    answer = (value >= 0 && value <= 10) ||
+    (value * 2 < 20 && value - 2 > -2) ||
+    (value - 1 > 1 && value / 2.0 < 10) ||
+    (value == 111);
+    
+    cout << (answer ? "ЦЕ ПРАВДА" : "ЦЕ НЕ ПРАВДА") << endl;
+    return 0;
+    
+    }
