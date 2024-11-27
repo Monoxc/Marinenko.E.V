0f725ab4aa37ef931ad07f2c80fe362081c724aa
Evgen M.V <marnenkozena@gmail.com>
2024-11-28 00:06:49 +0200

    Update main.cpp

--- C++/lab3/main.cpp
+++ C++/lab3/main.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+
+    setlocale(LC_ALL, "RU");
+
+    int N, product = 1;
+    bool even_found = false;
+
+    cout << "Введите трёхзначное число: ";
+    cin >> N;
+
+   
+    for (int i = 0; i < 3; ++i) { 
+        int digit = N % 10;
+        if (digit % 2 == 0) {
+            product *= digit;
+            even_found = true;
+
+        }
+        N /= 10;
+    }
+    
+   
+    if (even_found) {
+        cout << "Произведение парных цифр: " << product << endl;
+    }
+    else {
+        cout << "Парных цифр нет." << endl;
+    }
+
+    return 0;
+}
