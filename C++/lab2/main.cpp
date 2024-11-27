ccc9a5883a7b42cea17e76a756374242c49f40f0
Evgen M.V <marnenkozena@gmail.com>
2024-11-28 00:04:36 +0200

    Update main.cpp

--- C++/lab2/main.cpp
+++ C++/lab2/main.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+using namespace std;
+
+double calculateY(double x, double a, double c) {
+    double Y;
+
+
+    if (x <= 0) {
+        Y = 2 * x * x + a;
+    }
+    else {
+        Y = (x + 3 * a) * c;
+    }
+
+
+    return Y;
+}
+
+int main() {
+
+    setlocale(LC_ALL, "RU");
+    
+    double x = -3;
+    double a = 2.0;
+    double c = 3.0;
+  
+    double Y = calculateY(x, a, c);
+
+    cout << "Значення Y: " << Y << endl;
+
+    return 0;
+}
 
